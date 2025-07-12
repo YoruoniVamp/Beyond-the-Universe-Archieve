63c2212f697dc232d80454af84e6a5c62e66b9c4
Sangtongnaja <green8104@gmail.com>
2025-07-12 18:50:26 +0700

    Update MoneyApp.py
    อยากให้เพิ่มอะไรบอกได้จ้า

--- MoneyApp.py
+++ MoneyApp.py
@@ -5,11 +5,16 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox, QTableWidgetItem, QTableWidget
 )
 from PyQt5.QtGui import QFont
+from datetime import datetime
+
+now = datetime.now()
+current_date = now.strftime("%d-%m-%Y")
 
 class SangtongWallet(QWidget):
     def __init__(self):
         super().__init__()
         self.data = []
+        self.wallet_info = []
         self.setWindowTitle("WalletBySangtong")
         self.setGeometry(500, 500, 557, 500)
 
@@ -18,9 +23,11 @@ class SangtongWallet(QWidget):
         label_font = QFont("Segoe UI", 10, QFont.Bold)
         button_font = QFont("Segoe UI", 10)
 
-        main_layout = QVBoxLayout()
-        main_layout.setContentsMargins(20, 20, 20, 20)
-        main_layout.setSpacing(2)
+        main_layout = QHBoxLayout()
+
+        left_layout = QVBoxLayout()
+        left_layout.setContentsMargins(20, 20, 20, 20)
+        left_layout.setSpacing(2)
 
         label_income = QLabel("Income : ")
         label_income.setFont(label_font)
@@ -33,7 +40,7 @@ class SangtongWallet(QWidget):
         income_layout.addWidget(label_income)
         income_layout.addWidget(self.income_input)
         
-        main_layout.addLayout(income_layout)
+        left_layout.addLayout(income_layout)
 
         label_outcome = QLabel("Outcome :")
         label_outcome.setFont(label_font)
@@ -45,7 +52,7 @@ class SangtongWallet(QWidget):
         outcome_layout.addWidget(label_outcome)
         outcome_layout.addWidget(self.outcome_input)
         
-        main_layout.addLayout(outcome_layout)
+        left_layout.addLayout(outcome_layout)
 
         label_percentage = QLabel("Passive Income Percent :")
         label_percentage.setFont(label_font)
@@ -57,33 +64,47 @@ class SangtongWallet(QWidget):
         percentage_layout.addWidget(label_percentage)
         percentage_layout.addWidget(self.percentage_input)
 
-        main_layout.addLayout(percentage_layout)
+        left_layout.addLayout(percentage_layout)
+        
+        label_reason = QLabel("Reason :")
+        label_reason.setFont(label_font)
+        self.reason_input = QLineEdit()
+        self.reason_input.setPlaceholderText("เหตุผล")
+        self.reason_input.setStyleSheet("background-color: #7EC636; color: #FFFFFF; border: 1px solid #44aa44; padding: 5px;")
+
+        reason_layout = QHBoxLayout()
+        reason_layout.addWidget(label_reason)
+        reason_layout.addWidget(self.reason_input)
+
+        right_layout = QVBoxLayout()
+        right_layout.addLayout(reason_layout)
 
         self.table = QTableWidget()
         self.table.setColumnCount(5)
+        self.table.setFixedSize(517,250)
         self.table.setHorizontalHeaderLabels(["Month", "Total", "Income", "Outcome", "Passive"])
         self.table.setStyleSheet("""
             QTableWidget {
-                background-color: #71a300;
+                background-color: #5BB450;
                 color: #FCF8E8;
                 gridline-color: #000000;
             }
             QHeaderView::section {
-                background-color: #558B2F;
+                background-color: #175022;
                 color: white;
                 font-weight: bold;
             }
             QTableWidget QTableCornerButton::section {
-                background-color: #558B2F;
+                background-color: #175022;
             }
         """)
 
         self.table.setRowCount(0)
 
-        main_layout.addWidget(self.table)
+        left_layout.addWidget(self.table)
 
         button_layout = QHBoxLayout()
-        self.start_button = QPushButton("คำนวณ")
+        self.start_button = QPushButton("EndMonth")
 
         green_btn_style ="""
             QPushButton {
@@ -98,10 +119,80 @@ class SangtongWallet(QWidget):
         self.start_button.setStyleSheet(green_btn_style)
         self.start_button.clicked.connect(self.calculate_total)
 
-        main_layout.addWidget(self.start_button)
+        self.update_button = QPushButton("Income Update")
+        self.update_button.setStyleSheet(green_btn_style)
+        self.update_button.clicked.connect(self.update_income)
 
+        self.outcome_button = QPushButton("Outcome Update")
+        self.outcome_button.setStyleSheet(green_btn_style)
+        self.update_button.clicked.connect(self.update_outcome)
+        
+        button_layout.addWidget(self.start_button)
+        button_layout.addWidget(self.update_button)
+        button_layout.addWidget(self.outcome_button)
+        left_layout.addLayout(button_layout)
+
+        self.load_data()
+        for entry in self.data:
+            self.add_row_to_table(
+                entry.get("month", len(self.data)),
+                entry.get("total", 0),
+                entry.get("income", 0),
+                entry.get("outcome", 0),
+                entry.get("passive", 0),
+            )
+
+        main_layout.addLayout(left_layout, 2)
+
+        self.right_table = QTableWidget()
+        self.right_table.setColumnCount(3)
+        self.right_table.setFixedSize(452, 350)
+        self.right_table.setHorizontalHeaderLabels(["Income/Outcome", "Date", "Reason"])
+        self.right_table.setColumnWidth(0, 120)
+        self.right_table.setColumnWidth(1, 80)
+        self.right_table.setColumnWidth(2, 250)
+        self.right_table.setStyleSheet("""
+            QTableWidget {
+                background-color: #5BB450;
+                color: #FCF8E8;
+                gridline-color: #000000;
+            }
+            QHeaderView::section {
+                background-color: #175022;
+                color: white;
+                font-weight: bold;
+            }
+            QTableWidget QTableCornerButton::section {
+                background-color: #175022;
+            }
+        """)
+
+        right_layout.addWidget(self.right_table)
+
+        main_layout.addLayout(right_layout, 1)
+        
         self.setLayout(main_layout)
 
+    def load_data(self):
+        try:
+            with open("Money.json", "r", encoding="utf-8") as f:
+                self.data = json.load(f)
+        except FileNotFoundError:
+            self.data = [{
+                "month": 1,
+                "income": 0,
+                "outcome": 0,
+                "passive": 0,
+                "total": 0,
+            }]
+    
+    def load_walletdata(self):
+        try:    
+            with open("walletInfo.json", "r", encoding="utf-8") as f:
+                self.wallet_info = json.load(f)
+        except FileNotFoundError:
+            self.wallet_info = []
+    
     def get_last_total(self):
         if not self.data:
             try:
@@ -109,6 +200,16 @@ class SangtongWallet(QWidget):
             except:
                 return 0
         return self.data[-1]["total"]
+    
+    def get_last_income(self):
+        if not self.data:
+                return 0
+        return self.data[-1]["income"]
+    
+    def get_last_outcome(self):
+        if not self.data:
+                return 0
+        return self.data[-1]["outcome"]
     
     def add_row_to_table(self, month, total, income, outcome, passive=0):
         row = self.table.rowCount()
@@ -118,6 +219,90 @@ class SangtongWallet(QWidget):
         self.table.setItem(row, 2, QTableWidgetItem(str(income)))
         self.table.setItem(row, 3, QTableWidgetItem(str(outcome)))
         self.table.setItem(row, 4, QTableWidgetItem(str(passive)))
+    
+    def update_outcome(self):
+        try:
+            outcome = int(self.outcome_input.text() or 0)
+            reason = self.reason_input.text()
+            if not self.data:
+                QMessageBox.warning(self, "NoInfo", "Pls StartNewMonth")
+                return
+            last_entry = self.data[-1]
+            last_entry["outcome"] += outcome
+            last_entry["total"] = last_entry["total"] - outcome
+
+            self.save_data()
+
+            row = self.table.rowCount() - 1
+            self.table.setItem(row, 1, QTableWidgetItem(str(last_entry["total"])))
+            self.table.setItem(row, 3, QTableWidgetItem(str(last_entry["outcome"])))
+
+            now = datetime.now()
+            date_str = now.strftime("%d-%m-%Y")
+
+            new_row = self.right_table.rowCount()
+            self.right_table.insertRow(new_row)
+            self.right_table.setItem(new_row, 0, QTableWidgetItem(f"-{outcome}"))
+            self.right_table.setItem(new_row, 1, QTableWidgetItem(date_str))
+            self.right_table.setItem(new_row, 2, QTableWidgetItem(str(reason)))
+
+            self.wallet_info.append({
+                "type": "outcome",
+                "amount": outcome,
+                "date": date_str,
+                "reason": reason
+            })
+
+            self.save_walletdata()
+
+            self.outcome_input.clear()
+            self.reason_input.clear()
+        
+        except ValueError:
+            QMessageBox.warning(self, "ข้อมูลไม่ถูกต้อง", "กรอกข้อมูลให้ถูกต้อง")
+
+    def update_income(self):
+        try:
+            income = int(self.income_input.text() or 0)
+            reason = self.reason_input.text()
+
+            if not self.data:
+                QMessageBox.warning(self, "NoInfo", "Pls StartNewMonth")
+                return
+            
+            last_entry = self.data[-1]
+            last_entry["income"] += income
+            last_entry["total"] = last_entry["total"] + income
+
+            self.save_data()
+            
+            row = self.table.rowCount() - 1
+            self.table.setItem(row, 1, QTableWidgetItem(str(last_entry["total"])))
+            self.table.setItem(row, 2, QTableWidgetItem(str(last_entry["income"])))
+
+            now = datetime.now()
+            date_str = now.strftime("%d-%m-%Y")
+
+            new_row = self.right_table.rowCount()
+            self.right_table.insertRow(new_row)
+            self.right_table.setItem(new_row, 0, QTableWidgetItem(f"+{income}"))
+            self.right_table.setItem(new_row, 1, QTableWidgetItem(date_str))
+            self.right_table.setItem(new_row, 2, QTableWidgetItem(str(reason)))
+
+            self.wallet_info.append({
+                "type": "income",
+                "amount": income,
+                "date": date_str,
+                "reason": reason
+            })
+
+            self.save_walletdata()
+            
+            self.income_input.clear()
+            self.reason_input.clear()
+
+        except ValueError:
+            QMessageBox.warning(self, "ข้อมูลไม่ถูกต้อง", "กรอกข้อมูลให้ถูกต้อง")
 
     def calculate_total(self):
         try:
@@ -130,21 +315,32 @@ class SangtongWallet(QWidget):
             new_total = last_total + income - outcome + passive
             month = len(self.data) + 1
 
-            self.data.append ({
-                "month": month,
-                "total": new_total,
+            new_entry = {
+                "month" : month,
                 "income": income,
                 "outcome": outcome,
                 "passive": passive,
-            })
+                "total": new_total
+            }
 
-            self.add_row_to_table(month, new_total, income, outcome, passive)
+            self.data.append(new_entry)
+            self.save_data()
+            self.add_row_to_table(**new_entry)
             
             self.income_input.clear()
             self.outcome_input.clear()
             self.percentage_input.clear()
+            
         except ValueError:
             QMessageBox.warning(self, "ข้อมูลไม่ถูกต้องนะ", "กรอกตัวเลขใหม่ให้ถูก")
+   
+    def save_data(self):
+        with open("Money.json", "w", encoding="utf-8") as f:
+            json.dump(self.data, f, indent=2, ensure_ascii=False)
+
+    def save_walletdata(self):
+        with open("walletInfo.json", "w", encoding="utf-8") as f:
+            json.dump(self.wallet_info, f, indent=2, ensure_ascii=False)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
